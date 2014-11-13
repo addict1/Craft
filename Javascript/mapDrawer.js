e3c17dbde3b218d2b8744f783a05e2688206275d
addict1 <tristan_charpentier@hotmail.com>
2014-11-13 18:50:29 -0500

    Update mapDrawer.js

--- Javascript/mapDrawer.js
+++ Javascript/mapDrawer.js
@@ -4,8 +4,6 @@ var context=canvas.getContext('2D')
 var grass = new Image()
 grass.src="Pics/grass.png";
 
-
-
 var mapOrganized = map.split(";");
 var mapSuperOrganized = new Array();
 
@@ -15,4 +13,4 @@ for(var i = 0; i<mapOrganized.length; i++){
 
 function getTerrain(x,y){
 	return mapSuperOrganized[x][y];
-}
+} 
