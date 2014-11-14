ae6cc0c9b27740697a33340629a929d5800e3786
addict1 <tristan_charpentier@hotmail.com>
2014-11-13 19:23:39 -0500

    Update mapDrawer.js

--- Javascript/mapDrawer.js
+++ Javascript/mapDrawer.js
@@ -11,6 +11,28 @@ for(var i = 0; i<mapOrganized.length; i++){
 	mapSuperOrganized.push(mapOrganized[i].split(","));
 }
 
-function getTerrain(x,y){
+function getTile(x,y){
 	return mapSuperOrganized[x][y];
 } 
+
+function blocks2pixels (blocks){
+	return block*16;
+}
+function pixels2blocks (pixels){
+	return block/16;
+}
+function drawTile(image, x, y){
+	context.drawImage(image, x, y, image.width.image.height);
+}
+
+function drawTerrain(level){
+	for(var i = 0; i < level.length; i++){
+		for(var ii = 0; ii < level[i].length; i++){
+			var img = new Image();
+			if(tile[i][ii] == "grass"){
+				img = grass;
+			}
+			drawTile(img, i, ii);
+		}
+	}
+}
